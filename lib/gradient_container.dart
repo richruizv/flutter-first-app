91a352ac86393547201e2e0ef4a0fe76e31ad068
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-06-18 10:54:09 -0600

    random number generator

--- lib/gradient_container.dart
+++ lib/gradient_container.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
-import './styled_text.dart';
+import './dice_roller.dart';
 
 class GradientContainer extends StatelessWidget {
   final List<Color> colors;
 
   const GradientContainer(this.colors, {key}): super(key: key);
 
-  void rollDice(){
-
-  }
 
   
   @override
@@ -18,22 +15,7 @@ class GradientContainer extends StatelessWidget {
             gradient: LinearGradient(colors: colors)
             ),
           child: Center(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                Image.asset('assets/images/dice-5.png', width: 200,),
-                const SizedBox(height: 20),
-                TextButton(
-                  onPressed: rollDice, 
-                  style: TextButton.styleFrom(
-                    padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 20),
-                    foregroundColor: Colors.white,
-                    textStyle: const TextStyle(fontSize: 28)
-                    ),
-                  child: const Text('Roll Dice')
-                  )
-              ],
-            ),
+            child: DiceRoller()
           ),
         );
   }
