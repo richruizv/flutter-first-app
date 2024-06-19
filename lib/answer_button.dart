111078f25cb895bdcdd5092a59559cb3dc86970c
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-06-19 13:27:28 -0600

    creation answer component create question model and data

---
+++ lib/answer_button.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class AnswerButton extends StatelessWidget {
+  const AnswerButton({required this.onPressed, required this.text, super.key});
+
+  final void Function() onPressed;
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: onPressed,
+        style: ElevatedButton.styleFrom(
+            padding: const EdgeInsets.symmetric(vertical: 10, horizontal: 40),
+            backgroundColor: const Color.fromARGB(255, 33, 1, 95),
+            foregroundColor: Colors.white,
+            shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(40))),
+        child: Text(text));
+  }
+}
--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/answer_button.dart';
+import 'package:flutter_test_app/data/questions.dart';
 
-class QuestionScreen extends StatelessWidget {
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
 
   @override
+  State<QuestionScreen> createState() {
+    return _QuestionScreenState();
+  }
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  @override
   Widget build(context) {
-    return const Text('Hello Little one');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(currentQuestion.text, style: const TextStyle(color: Colors.white)),
+        const SizedBox(height: 30),
+        ...currentQuestion.answers.map((answer) {
+          return AnswerButton(onPressed: () {}, text: answer);
+        })
+      ]),
+    );
   }
 }
