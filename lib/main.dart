8db45532e48a64abf5da5ce4785d8f4f006867a0
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-06-18 19:14:52 -0600

    start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/start_screen.dart';
 
 void main() {
-  runApp(const MainApp());
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
+  runApp(
+    MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
+        body: Container(
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(colors: [
+              Color.fromARGB(255, 78, 13, 151),
+              Color.fromARGB(255, 107, 15, 68),
+            ], begin: Alignment.topLeft, end: Alignment.bottomRight)),
+            child: StartScreen()),
       ),
-    );
-  }
+    ),
+  );
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(context) {
+    return Center(
+        child: Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        // This also could be declared with the constructor Image.asset()
+        const Image(
+          image: AssetImage('images/quiz-logo.png'),
+          width: 200,
+        ),
+        const SizedBox(height: 80),
+        const Text(
+          'Learn Flutter the fun way!',
+          style: TextStyle(color: Colors.white, fontSize: 24),
+        ),
+        const SizedBox(height: 30),
+        OutlinedButton(
+            onPressed: () {
+              print('its way too late');
+            },
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            child: const Text(
+              'Start quiz',
+            ))
+      ],
+    ));
+  }
+}
