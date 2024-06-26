749094a5fe4916f44de3e6db0bd3fe66e090ba7d
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-06-25 19:11:00 -0600

    using color schemes

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test_app/widgets/expenses.dart';
 
+var kColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 96, 59, 181));
 void main() {
-  runApp(const MaterialApp(home: Expenses()));
+  runApp(MaterialApp(
+      theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+              backgroundColor: kColorScheme.onPrimaryContainer,
+              foregroundColor: kColorScheme.primaryContainer)),
+      home: const Expenses()));
 }
