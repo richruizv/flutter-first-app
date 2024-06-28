446f9808ca1ff6ae81d96f41b2d4c0f5be25094f
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-06-28 14:19:49 -0600

    theming app

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,17 @@ void main() {
           colorScheme: kColorScheme,
           appBarTheme: const AppBarTheme().copyWith(
               backgroundColor: kColorScheme.onPrimaryContainer,
-              foregroundColor: kColorScheme.primaryContainer)),
+              foregroundColor: kColorScheme.primaryContainer),
+          cardTheme: const CardTheme().copyWith(
+              color: kColorScheme.secondaryContainer,
+              margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+              style: ElevatedButton.styleFrom(
+                  backgroundColor: kColorScheme.primaryContainer)),
+          textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: kColorScheme.onSecondaryContainer,
+                  fontSize: 14))),
       home: const Expenses()));
 }
