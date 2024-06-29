b0d696661131091b6f06fc2a62fadd0c22424b16
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-06-28 18:50:27 -0600

    expense bucket class

--- lib/models/expense.dart
+++ lib/models/expense.dart
@@ -46,3 +46,21 @@ class Expense {
     return formatter(date);
   }
 }
+
+class ExpenseBucket {
+  const ExpenseBucket({required this.category, required this.expenses});
+
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  final Category category;
+  final List<Expense> expenses;
+
+  double get totalExpenses {
+    return expenses
+        .map((expense) => expense.amount)
+        .reduce((value, amount) => value + amount);
+  }
+}
