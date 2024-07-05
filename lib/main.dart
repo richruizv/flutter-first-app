f3ebe6096fb86c6de13b168bab5528887f735915
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-07-04 20:53:06 -0600

    aspect ratio

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/categories_screen.dart';
 
 import 'package:google_fonts/google_fonts.dart';
 
@@ -20,6 +21,6 @@ class App extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: theme, home: const Text('Hello whats up'));
+    return MaterialApp(theme: theme, home: const CategoriesScreen());
   }
 }
