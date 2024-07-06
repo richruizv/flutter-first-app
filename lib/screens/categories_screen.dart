7ca5ffbe608f1ce3e5a77730a6f11c3144b38da5
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-07-05 19:51:07 -0600

    making widget tappable with inkWell

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -11,6 +11,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
         appBar: AppBar(title: const Text('Pick your category')),
         body: GridView(
+            padding: const EdgeInsets.all(24),
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 2,
                 childAspectRatio: 1.5,
