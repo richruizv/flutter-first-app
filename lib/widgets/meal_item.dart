756fad1fb81d55888d84230cbcf65d6cf9a8b00d
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-07-05 21:02:37 -0600

    add positioned widget to my meal item style texts image cover and fixed height

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -11,12 +11,43 @@ class MealItem extends StatelessWidget {
   Widget build(BuildContext context) {
     // TODO: implement build
     return Card(
+      margin: const EdgeInsets.all(16),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(8)),
+      clipBehavior: Clip.hardEdge,
+      elevation: 2,
       child: InkWell(
           onTap: () {},
           child: Stack(children: [
             FadeInImage(
-                placeholder: MemoryImage(kTransparentImage),
-                image: NetworkImage(meal.imageUrl))
+              placeholder: MemoryImage(kTransparentImage),
+              image: NetworkImage(meal.imageUrl),
+              fit: BoxFit.cover,
+              height: 200,
+              width: double.infinity,
+            ),
+            Positioned(
+                bottom: 0,
+                left: 0,
+                right: 0,
+                child: Container(
+                  color: Colors.black54,
+                  padding:
+                      const EdgeInsets.symmetric(vertical: 6, horizontal: 44),
+                  child: Column(children: [
+                    Text(meal.title,
+                        maxLines: 2,
+                        textAlign: TextAlign.center,
+                        softWrap: true,
+                        overflow: TextOverflow.ellipsis,
+                        style: const TextStyle(
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20)),
+                    const SizedBox(
+                      height: 16,
+                    )
+                  ]),
+                ))
           ])),
     );
   }
