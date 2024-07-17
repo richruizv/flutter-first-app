dc83af4816d97be513152c4c12170ce448734176
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-07-17 17:39:58 -0600

    init flutter_provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_test_app/screens/categories_screen.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test_app/screens/tabs_screen.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -13,7 +13,9 @@ final theme = ThemeData(
 );
 
 void main() {
-  runApp(const App());
+  runApp(const ProviderScope(
+    child: App(),
+  ));
 }
 
 class App extends StatelessWidget {
--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -1,9 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_test_app/models/meal.dart';
 import 'package:flutter_test_app/screens/categories_screen.dart';
 import 'package:flutter_test_app/screens/filters_screen.dart';
 import 'package:flutter_test_app/screens/meals_screen.dart';
-import 'package:flutter_test_app/data/dummy_data.dart';
+import 'package:flutter_test_app/providers/meals_provider.dart';
 import 'package:flutter_test_app/widgets/main_drawer.dart';
 
 const kInitialFilters = {
@@ -13,16 +14,16 @@ const kInitialFilters = {
   Filter.vegan: false
 };
 
-class TabsScreen extends StatefulWidget {
+class TabsScreen extends ConsumerStatefulWidget {
   const TabsScreen({super.key});
 
   @override
-  State<StatefulWidget> createState() {
+  ConsumerState<TabsScreen> createState() {
     return _TabsScreenState();
   }
 }
 
-class _TabsScreenState extends State<TabsScreen> {
+class _TabsScreenState extends ConsumerState<TabsScreen> {
   int _selectedPageIndex = 0;
   final List<Meal> _favoriteMeals = [];
   var _selectedFilters = kInitialFilters;
@@ -74,7 +75,8 @@ class _TabsScreenState extends State<TabsScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final availableMeals = dummyMeals.where((meal) {
+    final meals = ref.watch(mealsProvider);
+    final availableMeals = meals.where((meal) {
       if (_selectedFilters[Filter.glutenFree]! && !meal.isGlutenFree) {
         return false;
       }
