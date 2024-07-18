8e2a0adaa393a7b189ae41256d556de092be2315
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2024-07-18 11:28:32 -0600

    initial auth form

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_test_app/screens/auth_screen.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +10,12 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return MaterialApp(
+        title: 'FlutterChat',
+        theme: ThemeData().copyWith(
+          colorScheme: ColorScheme.fromSeed(
+              seedColor: const Color.fromARGB(255, 63, 17, 177)),
         ),
-      ),
-    );
+        home: const AuthScreen());
   }
 }
